74386d069d085ce26434ddd4e18e2136afe8da35
imamansari643 <144089021+imamansari643@users.noreply.github.com>
2024-05-06 20:14:57 +0530

    added channel name

--- src/components/Navbar/ResponsiveMenu.jsx
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -9,8 +9,7 @@ const ResponsiveMenu = ({ showMenu, setShowMenu }) => {
     <div
       className={`${
         showMenu ? "left-0" : "-left-[100%]"
-      } fixed bottom-0 top-0 z-20 flex h-screen w-[75%] flex-col justify-between bg-white dark:bg-gray-900 dark:text-white px-8 pb-6 pt-16 text-black transition-all duration-200 md:hidden rounded-r-xl shadow-md`}
-    >
+      } fixed bottom-0 top-0 z-20 flex h-screen w-[75%] flex-col justify-between bg-white dark:bg-gray-900 dark:text-white px-8 pb-6 pt-16 text-black transition-all duration-200 md:hidden rounded-r-xl shadow-md`}>
       <div className="card">
         <div className="flex items-center justify-start gap-3">
           <FaUserCircle size={50} />
@@ -26,8 +25,7 @@ const ResponsiveMenu = ({ showMenu, setShowMenu }) => {
                 <Link
                   to={data.link}
                   onClick={() => setShowMenu(false)}
-                  className="mb-5 inline-block"
-                >
+                  className="mb-5 inline-block">
                   {data.name}
                 </Link>
               </li>
@@ -37,7 +35,8 @@ const ResponsiveMenu = ({ showMenu, setShowMenu }) => {
       </div>
       <div className="footer">
         <h1>
-          Made with ❤ by <a href="https://dilshad-ahmed.github.io/">Dilshad</a>{" "}
+          Made with ❤ by{" "}
+          <a href="https://github.com/imamansari643/travel-website2.0">imam</a>{" "}
         </h1>
       </div>
     </div>
